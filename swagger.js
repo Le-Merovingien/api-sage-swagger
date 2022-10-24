69c49c613916599d1c64e58c3129f1ba052eb8a5
Yann <yann@merovingien.fr>
2022-10-24 18:23:31 +0200

    feat configuration

--- swagger.js
+++ swagger.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   const ui = SwaggerUIBundle({
     url: "swagger.yaml",
+    configUrl: 'config.json',
     dom_id: '#swagger-ui',
     presets: [
       SwaggerUIBundle.presets.apis,
